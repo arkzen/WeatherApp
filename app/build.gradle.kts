879f7e207f58fe511da892d292e1b31d2b9af8c4
Asikur <asikurr817@gmail.com>
2024-06-05 09:34:55 +0600

    [update] dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,12 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id ("com.google.dagger.hilt.android")
+    id("dagger.hilt.android.plugin")
+    kotlin("kapt")
+
 }
+
 
 android {
     namespace = "codingtest.jatri.WeatherApp"
@@ -15,6 +20,10 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures{
+        viewBinding=true
     }
 
     buildTypes {
@@ -37,6 +46,13 @@ android {
 
 dependencies {
 
+
+    val hilt_version = "2.48.1"
+    val lifecycle_version ="2.6.2"
+    val retrofit_version ="2.9.0"
+    val coroutine_version="1.7.3"
+
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -45,4 +61,33 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
+
+    // Hilt
+    implementation("com.google.dagger:hilt-android:$hilt_version")
+    kapt("com.google.dagger:hilt-android-compiler:$hilt_version")
+
+
+    //Lifecycle
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycle_version")
+    implementation ("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation ("androidx.activity:activity-ktx:1.8.1")
+
+    //Coroutines
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutine_version")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutine_version")
+
+    //Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:$retrofit_version")
+    implementation ("com.squareup.retrofit2:converter-gson:$retrofit_version")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation ("com.squareup.retrofit2:converter-scalars:$retrofit_version")
+
+    //Reflection
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.10")
+
+    //SwipeRefreshlayout
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,22 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
+    id("com.google.dagger.hilt.android") version "2.48.1" apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+        maven {
+            url = uri("https://maven.fabric.io/public")
+        }
+
+    }
+    dependencies {
+        classpath("de.mannodermaus.gradle.plugins:android-junit5:1.8.2.1")
+        classpath("com.google.gms:google-services:4.4.2")
+    }
 }
