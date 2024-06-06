b3a90c0adf7ab2dc6adb021deb1a222edf1c9ffc
Asikur Rahman <asikurr817@gmail.com>
2024-06-06 19:01:56 +0600

    [update] weather details function in apiservice

--- app/src/main/java/codingtest/jatri/WeatherApp/api/ApiService.kt
+++ app/src/main/java/codingtest/jatri/WeatherApp/api/ApiService.kt
@@ -1,14 +1,25 @@
 package codingtest.jatri.WeatherApp.api
 
 import codingtest.jatri.WeatherApp.models.CityWeatherResponse
+import codingtest.jatri.WeatherApp.models.WeatherDetailsResponse
 import codingtest.jatri.WeatherApp.ui.CityLocationFragment
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
-    @GET("course/enroll-courses/")
+    @GET("find")
     suspend fun getCityWeatherList(
-        @Query("status") status: String
+        @Query("lat") latitude: Double,
+        @Query("lon") longitude: Double,
+        @Query("cnt") cnt: Int,
+        @Query("appid") appId: String,
     ): Response<CityWeatherResponse>
+
+    @GET("weather")
+    suspend fun getWeatherDetails(
+        @Query("lat") latitude: Double,
+        @Query("lon") longitude: Double,
+        @Query("appid") appId: String,
+    ): Response<WeatherDetailsResponse>
 }
